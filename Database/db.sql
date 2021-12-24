6c5f94928eb815374b753aea466ecb565b2a9f78
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-12-25 00:31:11 +0600

    update db sql file

--- Database/db.sql
+++ Database/db.sql
@@ -5,10 +5,10 @@ use football_club_management
 create table users
 	(
 		name varchar(30) not null,
-		email_address varchar(40), not null,
-		user_id varchar(20), not null,
-		password varchar(30), not null,
-		phone_number int(13), not null,
-		region varchar(20), not null,
-		date_of_birth int(8), not null 
+		email_address varchar(40) not null,
+		user_id varchar(20) not null,
+		password varchar(30) not null,
+		phone_number int(13) not null,
+		region varchar(20) not null,
+		date_of_birth int(8) not null 
 		);
