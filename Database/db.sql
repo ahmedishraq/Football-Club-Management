2b75699a826a197784a67ebe6d97a106a808ef7d
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-12-25 00:22:30 +0600

    initial update of db sql file

---
+++ Database/db.sql
@@ -0,0 +1,14 @@
+create database football_club_anagement
+
+use football_club_management
+
+create table users
+	(
+		name varchar(30) not null,
+		email_address varchar(40), not null,
+		user_id varchar(20), not null,
+		password varchar(30), not null,
+		phone_number int(13), not null,
+		region varchar(20), not null,
+		date_of_birth int(8), not null 
+		);
