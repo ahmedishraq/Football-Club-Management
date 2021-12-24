cf95d404c5cd6e6093927c610f6bd23e13ea2d44
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-12-25 00:39:33 +0600

    insert data update in sql

--- Database/db.sql
+++ Database/db.sql
@@ -12,3 +12,8 @@ create table users
 		region varchar(20) not null,
 		date_of_birth int(8) not null 
 		);
+
+insert into users
+	(
+		"Ishraq Ahmed","ishraqahmedbd@gmail.com","ahmed_ishraq","1234",01534741641,"Dhaka","07-04-1999"
+		);
