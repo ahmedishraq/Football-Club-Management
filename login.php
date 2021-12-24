a11fcd9c9281fdbd5659d74e904702f3314e1b68
ahmed_ishraq <ishraqahmedbd@gmail.com>
2021-12-25 00:58:18 +0600

    minor update

--- login.php
+++ login.php
@@ -21,16 +21,16 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+		$sql = "SELECT * FROM users WHERE user_id='$uname' AND password='$pass'";
 
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-            if ($row['user_name'] === $uname && $row['password'] === $pass) {
-            	$_SESSION['user_name'] = $row['user_name'];
+            if ($row['user_id'] === $uname && $row['password'] === $pass) {
+            	$_SESSION['user_id'] = $row['user_id'];
             	$_SESSION['name'] = $row['name'];
-            	$_SESSION['id'] = $row['id'];
+            	// $_SESSION['id'] = $row['id'];
             	header("Location: index.html");
 		        exit();
             }else{
