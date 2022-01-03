0e8c62faf04cc4004da4294f4a9009695e5293ba
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2022-01-04 00:27:29 +0600

    database sql update

--- index.php
+++ index.php
@@ -3,68 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Manchester United</title>
-    <style>
-      body{
-    	 font-family:"Arial", sans-serif;
-    	 margin:0px;
-		  background-color: white;
-      }
-
-      header{
-        font-family: "Arial" , sans-serif;
-        text-align: center;
-        background-image: url("https://drive.google.com/uc?export=view&id=1f7N-8LcI2PVpLm7LRHWtPtZ-yJCX-qKc");
-        color: #b30000;
-        padding:250px;
-        font-size: 50px;
-        }
-   
- nav{
-  background-color: #b30000;
-  overflow: hidden;
-  }
-
-  nav ul{
-  	list-style: none;
-  	padding: 0px;
-  	margin: 0px;
-
-  }
-  nav ul li{
-  	float: left;
-
-  }
-  nav ul li a{
-  	text-decoration: none;
-  	color:white;
-  	padding: 10px 25px;
-  	display: inline-block;
-  	font-size: 18px;
-
-  }
-  nav ul li a:hover{
-  	background-color: black;
-    color: #ffffff;
-  }
-  nav ul li a.active{
-  	background-color: grey;
-  }
-
-  .title{
-    background-image: url("https://drive.google.com/uc?export=view&id=1iSeV7imNZgWfMXyB58lwARElfbQ44TDV");
-    width: 100%;
-    padding: 12px 20px;
-    margin-top: 40px;
-    text-align:center;
-    text-transform: Uppercase;
-    font-weight: 800;
-    font-size: 30px;
-  }
-
-  </style>
+    <link rel="stylesheet" type="text/css" href="style.css">
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 <!-- main header -->
@@ -84,3 +29,49 @@
   <li><b><a href="admin.php">ADMIN</a></b></li>
 </ul>
 </nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img style="margin-left:20%;margin-right:18%;" src="images/pic2.png" alt="Home Kit" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img style="margin-left:20%;margin-right:18%;" src="images/pic3.png" alt="Christmas" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img style="margin-left:20%;margin-right:18%;" src="images/pic4.png" alt="Inside View" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+
+
+<!-- main footer -->
+</section>
+<footer>
+<h5 style="color:black" align="center";>Copyright &copy;2021-2022 , Manchester United</h5>
+<p style="color:red" align="center";>All rights are reserved.</p>
+<small style="color:black">Developed & maintained by Mustakin, Wakil, Sababa, Ishraq, Bidhan</small>
+</footer>
+
+
+</body>
+</html>
