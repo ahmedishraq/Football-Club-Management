6b97e86de10dfdd26ffa4ce6399f81e175b6401f
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2022-01-04 00:35:07 +0600

    database sql update

--- Database/db.sql
+++ Database/db.sql
@@ -13,7 +13,27 @@ create table users
 		date_of_birth int(8) not null 
 		);
 
-insert into users values
+create table board
 	(
-		"Ishraq Ahmed","ishraqahmedbd@gmail.com","ahmed_ishraq","1234",01534741641,"Dhaka","07-04-1999"
+		member_id varchar(20),
+		name varchar(40),
+		joining_date date
 		);
+
+create table shop_staff
+	(
+		staff_id varchar(10),
+		name varchar(40),
+		email varchar(50),
+		salary varchar(20),
+		supervisor varchar(15)
+		);
+
+insert into board (member_id, name, joining_date) values
+('m01', 'Avram Gui', '2020-06-18'),
+('m02', 'John Hooks ', '2019-04-13'),
+('m03', 'Cliff Bata', '2019-04-20'),
+('m04', 'Kevin Good', '2021-01-29'),
+('m05', 'Richard Arnold ', '2020-03-06'),
+('m06', 'Josh Hazel', '2021-05-11'),
+('m07', 'Saby Raida', '2021-09-22');
